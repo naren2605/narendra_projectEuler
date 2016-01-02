8cb2d6716b6505d10c75126726a830092994e8f3
narendra kumar <naren26051991@gmail.com>
2016-01-02 17:20:25 +0530

    started solving problem 13..

---
+++ src/main/java/dev/euler/prob13/LargeSum.java
@@ -0,0 +1,8 @@
+package dev.euler.prob13;
+
+public class LargeSum {
+	public static void main(String[] args) {
+String data="067166682355524525280460972253503534226472524250874054075591789781264330331690";
+
+	}
+}
