236a077e49afdb00a13e2731987dc4205fe7f143
narendra kumar <naren26051991@gmail.com>
2016-01-04 08:09:16 +0530

    problem 13 solved - sum of hundered 50 digit numbers

--- src/main/java/dev/Utils.java
+++ src/main/java/dev/Utils.java
@@ -1,6 +1,10 @@
 package dev;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Collections;
 
 import dev.euler.prob23.NonAbundantSums;
 import dev.euler.prob3.LargestPrimeNumberFactor;
@@ -39,4 +43,112 @@ public class Utils {
 		list.add(number);
 		return list;
 	}
+	
+	
+    public static String add(int[][] data){
+    	int rlen=data.length;
+    	int clen=data[0].length;
+    	ArrayList<Integer> sumList=new ArrayList<Integer>(); 
+    	for(int i=0;i<clen;i++){
+    		int sum=0;
+    		for(int j=0;j<rlen;j++){
+    			sum=sum+data[j][i];
+    			
+    		}
+    		
+    		sumList.add(sum);
+    	}
+    	
+    	Collections.reverse(sumList);
+    	int overFlowSum=0;
+		for(int i=0;i<sumList.size();i++){
+			if(i+1==sumList.size()&&(sumList.get(i)/10>0)){
+				int overflow=sumList.get(i)/10;
+				int number=sumList.get(i)%10;
+				sumList.set(i, number);
+				sumList.add(i+1, overflow);
+			}
+			else if(i+1<sumList.size()){
+				int overflow=sumList.get(i)/10;
+				int number=sumList.get(i)%10;
+				sumList.set(i, number);
+				sumList.set(i+1, overflow+sumList.get(i+1));
+			}
+			
+		}
+		
+		String sum="";
+		for(int i:sumList){
+			sum=i+sum;
+		}
+        return sum;		
+    }
+	
+	
+	public static String getStringDataFromFile(File file){
+		String data="";
+		try{
+			FileReader fr= new FileReader(file);
+			BufferedReader br = new BufferedReader(fr);
+		
+			String line="";
+			for(;(line=br.readLine())!=null;){
+				data=data+line;
+			}
+		}
+		catch(Exception e){
+			
+		}
+		return data;
+	}
+	
+	
+	public static int[][] convertStringData(String data,int numberLength){
+		int size=data.length()/numberLength;
+		if(data.length()%2!=0){
+			size=size+1;
+		}
+		int[][] sparseArray=new int[size][];
+		
+		for(int i=0,k=0;k<size;i=i+numberLength,k++){
+			if(i>data.length()){
+				
+				sparseArray[k]=convertStringDataToNumericData(data.substring(i-numberLength, data.length()), 1);
+			}
+			else{
+				
+				sparseArray[k]=convertStringDataToNumericData(data.substring(i, i+numberLength), 1);
+			
+	
+			}
+					}
+		
+		
+		return sparseArray;
+	}
+
+	
+	public static int[] convertStringDataToNumericData(String data,int numberLength){
+		byte[] dataBytes=data.getBytes();
+		int size=dataBytes.length/numberLength;
+		if(dataBytes.length%numberLength!=0){
+			size++;
+		}
+		int[] dataIntArray=new int[size];
+		int zero=(int)'0';
+		for(int i=0,k=0;i<dataBytes.length;i=i+numberLength,k++){
+		   int  localdata=-1;
+			for(int j=0;j<numberLength&&(i+j)<dataBytes.length;j++){
+	          if(localdata<0){
+	        	  localdata=0;
+	          }
+	          localdata=(localdata*10+(((int)dataBytes[i+j])-zero));
+			}
+			if(localdata>0){
+				
+				dataIntArray[k]=localdata;
+			}
+		}
+		return dataIntArray;
+	} 
 }
--- src/main/java/dev/euler/prob13/LargeSum.java
+++ src/main/java/dev/euler/prob13/LargeSum.java
@@ -1,8 +1,19 @@
 package dev.euler.prob13;
 
+import java.io.File;
+
+import dev.Utils;
+
 public class LargeSum {
 	public static void main(String[] args) {
-String data="067166682355524525280460972253503534226472524250874054075591789781264330331690";
-
+		File file= new File("problem13Data");
+	
+		
+		String dataString=Utils.getStringDataFromFile(file);
+		//String dataString="1224";
+		int[][] data=Utils.convertStringData(dataString, 50);
+	    System.out.println(Utils.add(data));
+		
+		
 	}
 }
