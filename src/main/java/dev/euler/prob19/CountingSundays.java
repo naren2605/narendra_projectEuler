be2053d9a78c945adb34487a9cedf45214d46525
Narendra.Kumar <naren26051991@gmail.com>
2016-01-11 16:39:02 +0530

    problem 19 solved

--- src/main/java/dev/euler/prob19/CountingSundays.java
+++ src/main/java/dev/euler/prob19/CountingSundays.java
@@ -30,6 +30,9 @@ public class CountingSundays {
 		public int daysOfMonth(int month,int year){
 			if(month==2){
 				if(year %400 ==0){
+					return 29;
+				}
+				else if(year %100 ==0){
 					return 28;
 				}
 				else if(year%4==0){
@@ -102,23 +105,24 @@ public class CountingSundays {
 public static void main(String[] args) {
 	
 	Date startDate1=new Date(1, 1, 1900,1);
-	Date endDate2=new Date(14,12,2015);
-
+	Date endDate2=new Date(13,1,1999);//2
 	
+
 	//System.out.println((effectiveDays(startDate1, endDate2)/7)-((effectiveDays(startDate1, endDate1)/7)));
-	System.out.println(getFutureDayFromPastDay(startDate1, endDate2));
+System.out.println(getFutureDayFromPastDay(startDate1, endDate2));
 	int count=0;
 	for(int i=1901;i<=2000;i++){
-		System.out.println("Year..."+i);
+		
 		for(int month=1;month<=12;month++){
-			System.out.println("month  "+month);
 			Date endDate1=new Date(1,month,i);
 			String day=getFutureDayFromPastDay(startDate1, endDate1);
+			System.out.println("day.."+day);
 			if(dayInfo.daysInteger.get(day)==7){
+				System.out.println("year = "+endDate1.year+"month = "+endDate1.month+"day = "+endDate1.day_month);
 				count++;
 			}
-			System.out.println("day=="+day);
-			startDate1=new Date(1, month, i, dayInfo.daysInteger.get(day));
+			
+			//startDate1=new Date(1, month, i, dayInfo.daysInteger.get(day));
 		}
 	}
 	System.out.println(count+" sundays");
@@ -129,21 +133,28 @@ public static String getFutureDayFromPastDay(Date past,Date future){
 	if(past.day!=0){
 
 		int days=effectiveDays(past, future);
-	//	System.out.println("days=="+days+"===="+(days%7));
+	
+		System.out.println("days=="+days+"===="+(days%7));
 	    if(days%7!=0){
 	    	int day=0;
 	    	
-	    		int diff=future.day_month-(days%7);
+	    	//	int diff=future.day_month-(days%7);
 	    		for(int i=1 ,k=past.day;i<=((days%7));i++){
-	    			if(k==7){
-	    				k=1;
-	    			}
-	    			else{
+	    		//	System.out.println(i);
+	    			
+	    			
 	    				k++;
-	    			}
-	    			day=k;
+	    				day=k%7;
+		    			
+		    			if(day==0){
+		    				k=7;
+		    				day=7;
+		    			}
+	    			
+	    			
 	    		}
-	    		return dayInfo.days.get(day);
+	    		System.out.println(day+"da....");
+	    		return dayInfo.days.get((day)%7==0?7:(day)%7);
 	    	
 	    }
 	    else{
@@ -161,11 +172,34 @@ public static int effectiveDays(Date start,Date end){
 	int totaldays=0;
 	
 	for(int year=start.year;year<=end.year;year++){
-
-		if(year==start.year){
+        if(start.year==end.year){
+        	int days=0;
+        	for(int  month=start.month;month<=end.month;month++){
+        		if(month==start.month&&month==end.month){
+        			days=days+(end.day_month-start.day_month);
+        			System.out.println(days);
+        		}
+        		else if(month==start.month){
+        			days=days+dayInfo.daysOfMonth(month, year)-start.day_month;
+        			System.out.println(days);
+        		}
+        		else if(month==end.month){
+        			
+        			days=days+end.day_month;
+        			//System.out.println(days);
+        		}
+        		else{
+        			days=days+dayInfo.daysOfMonth(month, year);
+        		}
+				
+			}
+			totaldays=totaldays+days;
+        } 
+        else if(year==start.year){
 			int days=0;
 			for(int  month=start.month;month<=12;month++){
 				days=days+dayInfo.daysOfMonth(month, year);
+				//System.out.println(dayInfo.daysOfMonth(month, year));
 			}
 			days=days-start.day_month;
 			totaldays=totaldays+days;
@@ -175,14 +209,20 @@ public static int effectiveDays(Date start,Date end){
 			int days=0;
 			for(int  month=1;month<end.month;month++){
 				days=days+dayInfo.daysOfMonth(month, year);
+				//System.out.println(dayInfo.daysOfMonth(month, year));
+				
 			}
 			days=days+end.day_month;
 			totaldays=totaldays+days;
 		}
 		else{
 			if(year%400==0){
+				totaldays=totaldays+366;
+				
+			}else if(year%100==0){
 				totaldays=totaldays+365;
-			}else if(year%4==0){
+			}
+			else if(year%4==0){
 				totaldays=totaldays+366;
 			}
 			else{
